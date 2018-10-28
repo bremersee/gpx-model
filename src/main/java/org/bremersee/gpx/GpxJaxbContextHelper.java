71cdb1bc9cf336d250af3c5fd5fea5540d3cf523
Christian Bremer <christian.bremer@cstx.de>
2018-10-28 02:42:13 +0100

    Init

--- src/main/java/org/bremersee/gpx/GpxJaxbContextHelper.java
+++ src/main/java/org/bremersee/gpx/GpxJaxbContextHelper.java
@@ -16,7 +16,17 @@
 
 package org.bremersee.gpx;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import org.bremersee.gpx.model.ExtensionsType;
 import org.bremersee.gpx.model.ObjectFactory;
+import org.w3c.dom.Element;
 
 /**
  * XML context helper.
@@ -68,6 +78,114 @@ public abstract class GpxJaxbContextHelper {
       }
     }
     return sb.toString();
+  }
+
+  @SuppressWarnings("WeakerAccess")
+  public static Map<Class<?>, List<Object>> parseExtensions(
+      final ExtensionsType extensions,
+      final JAXBContext jaxbContext) {
+    try {
+      return parseExtensions(extensions, jaxbContext.createUnmarshaller());
+    } catch (final Throwable ignored) {
+      return parseExtensions(extensions, (Unmarshaller) null);
+    }
+  }
+
+  @SuppressWarnings("WeakerAccess")
+  public static Map<Class<?>, List<Object>> parseExtensions(
+      final ExtensionsType extensions,
+      final Unmarshaller unmarshaller) {
+    final Map<Class<?>, List<Object>> map = new HashMap<>();
+    if (extensions == null || extensions.getAnies() == null) {
+      return map;
+    }
+    for (final Element element : extensions.getAnies()) {
+      if (element != null) {
+        final Object strictElement = parseElement(element, unmarshaller);
+        final List<Object> values = map.computeIfAbsent(
+            strictElement.getClass(), k -> new ArrayList<>());
+        values.add(strictElement);
+      }
+    }
+    return Collections.unmodifiableMap(map);
+  }
+
+  private static Object parseElement(final Element element, final Unmarshaller unmarshaller) {
+    try {
+      return unmarshaller.unmarshal(element);
+
+    } catch (final Throwable ignored) {
+      return element;
+    }
+  }
+
+  @SuppressWarnings("WeakerAccess")
+  public static <T> List<T> findExtensions(
+      final Class<T> cls,
+      final boolean instancesOf,
+      final Map<Class<?>, List<Object>> parsedExtensions) {
+
+    if (cls == null || parsedExtensions == null || parsedExtensions.isEmpty()) {
+      return Collections.emptyList();
+    }
+    final List<T> list = new ArrayList<>();
+    for (Map.Entry<Class<?>, List<Object>> entry : parsedExtensions.entrySet()) {
+      final Class<?> c = entry.getKey();
+      if (cls.equals(c) || (instancesOf && cls.isAssignableFrom(c))) {
+        //noinspection unchecked
+        list.addAll((List<T>) entry.getValue());
+      }
+    }
+    return Collections.unmodifiableList(list);
+  }
+
+  @SuppressWarnings("WeakerAccess")
+  public static <T> List<T> findExtensions(
+      final Class<T> cls,
+      final boolean instancesOf,
+      final ExtensionsType extensions,
+      final JAXBContext jaxbContext) {
+
+    return findExtensions(cls, instancesOf, parseExtensions(extensions, jaxbContext));
+  }
+
+  @SuppressWarnings("WeakerAccess")
+  public static <T> List<T> findExtensions(
+      final Class<T> cls,
+      final boolean instancesOf,
+      final ExtensionsType extensions,
+      final Unmarshaller unmarshaller) {
+
+    return findExtensions(cls, instancesOf, parseExtensions(extensions, unmarshaller));
+  }
+
+  public static <T> Optional<T> findFirstExtension(
+      final Class<T> cls,
+      final boolean instancesOf,
+      final Map<Class<?>, List<Object>> parsedExtensions) {
+
+    final List<T> list = findExtensions(cls, instancesOf, parsedExtensions);
+    return list.isEmpty() ? Optional.empty() : Optional.of(list.get(0));
+  }
+
+  public static <T> Optional<T> findFirstExtension(
+      final Class<T> cls,
+      final boolean instancesOf,
+      final ExtensionsType extensions,
+      final JAXBContext jaxbContext) {
+
+    final List<T> list = findExtensions(cls, instancesOf, extensions, jaxbContext);
+    return list.isEmpty() ? Optional.empty() : Optional.of(list.get(0));
+  }
+
+  public static <T> Optional<T> findFirstExtension(
+      final Class<T> cls,
+      final boolean instancesOf,
+      final ExtensionsType extensions,
+      final Unmarshaller unmarshaller) {
+
+    final List<T> list = findExtensions(cls, instancesOf, extensions, unmarshaller);
+    return list.isEmpty() ? Optional.empty() : Optional.of(list.get(0));
   }
 
 }
