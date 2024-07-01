c597d72d9b02f5bc4a06f58e25e2cf27c1197cd4
Jenkins User <jenkins@jenkins-agent-1.eixe.bremersee.org>
2024-07-02 00:03:59 +0200

    Site checkin for project GPX Model

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.bremersee.gpx"},{"l":"org.bremersee.gpx.model"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.bremersee.gpx"},{"l":"org.bremersee.gpx.model"}];updateSearchResults();
